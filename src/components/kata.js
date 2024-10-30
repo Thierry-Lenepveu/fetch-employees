50a2a73283e4d395b9f00d15bca291dfb0045c38
Thierry-Lenepveu <thierry.lenepveu.33@gmail.com>
2024-10-30 17:10:41 +0100

    use api from express.

--- src/components/kata.js
+++
@@ -1,9 +0,0 @@
-function isAmstrong(num) {
-    return num === num
-                    .toString()
-                    .split("")
-                    .map(value => parseInt(value))
-                    .reduce((sum, value, _, array) => sum + Math.pow(value, array.length), 0);
-}
-
-console.log(isAmstrong(153));
